1b69a4211836cd84ecadf23e81ee5b17067fba01
Zack Falgout <zfalgout@gmail.com>
2015-02-22 12:45:58 -0600

    production email bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,13 +82,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'gamesetplay.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+  
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
     :user_name      => 'app34065901@heroku.com',
     :password       => 'quoxkivo',
-    :domain         => 'heroku.com','app34065901@heroku.com'
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end
