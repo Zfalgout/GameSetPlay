258042f1670e68766de0d3b715c72fe4e4baa3b5
Zack Falgout <zfalgout@gmail.com>
2015-02-22 14:14:52 -0600

    Edited production.rb for security reasons.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,8 +87,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'app34065901@heroku.com',
-    :password       => 'quoxkivo',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
