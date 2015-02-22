9b12e2062b15ff83700b2671385507c2d28c6044
Zack Falgout <zfalgout@gmail.com>
2015-02-22 12:36:21 -0600

    working on email development evironment bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,9 +41,9 @@ Rails.application.configure do
 
   #Configuring the mailers.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'example.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 
 
 end
