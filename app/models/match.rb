d00a059f24e42d6f07e82111f3a7162d9da71420
unknown <zfalgout@DSMWIN8.helena.com>
2015-04-23 09:00:57 -0500

    Have multi-column search working through type + other 3.  Still need (creator, zip, date) + 3.

--- app/models/match.rb
+++ app/models/match.rb
@@ -25,22 +25,55 @@ class Match < ActiveRecord::Base
 
     end
 
-    def self.search(query1, query2)
+    def self.search(query1, query2, query3, query4)
     	@query1 = query1
     	@query2 = query2
 
       #Have to catch in case of nil.
-  		if (query1 != "" && query2 != "")
-  			where("game_type like ? AND player1 like ?", "%#{query1}%", User.find_by(name: "#{query2}").id)
-  			#where("game_type like ?", "%#{query1}%")
-  		elsif (query1 != "")
+  		if (query1 != "" && query2 != "" && query3 != "" && query4 != "") #A user uses all search criteria.
+  			where("game_type like ? AND player1 like ? AND zip like ? AND time like ?", "%#{query1}%", User.find_by(name: "#{query2}").id, "%#{query3}%", "%#{query4}%")
+  		elsif (query1 != "" && query2 == "" && query3 == "") #A user searches for a match by type.
   			where("game_type like ?", "%#{query1}%")
-  		elsif (query2 != "")
+  		
+  		elsif (query1 == "" && query2 != "" && query3 == "" && query4 == "") #A user searches for a match by creator
   			where("player1 like ?", User.find_by(name: "#{query2}").id)
+  		
+  		elsif (query1 == "" && query2 == "" && query3 != "" && query4 == "") #A user searches for a match by zip.
+  			where("zip like ?", "%#{query3}%")
+  		
+  		elsif (query1 == "" && query2 == "" && query3 == "" && query4 != "") #A user searches for a match by date.
+  			where("time like ?", "%#{query4}%")
+
+  		elsif (query1 != "" && query2 != "" && query3 == "" && query4 == "") #A user searches for a match by type and creator.
+  			where("game_type like ? AND player1 like ?", "%#{query1}%", User.find_by(name: "#{query2}").id)
+  		
+  		elsif (query1 != "" && query2 == "" && query3 != "" && query4 == "") #A user searches for a match by type and zip.
+  			where("game_type like ? AND zip like ?", "%#{query1}%", "%#{query3}%")
+
+  		elsif (query1 == "" && query2 != "" && query3 != "" && query4 == "") #A user searches for a match by creator and zip.
+  			where("player1 like ? AND zip like ?", User.find_by(name: "#{query2}").id, "%#{query3}%")
+
+  		elsif (query1 != "" && query2 == "" && query3 == "" && query4 != "") #A user searches for a match by type and date.
+  			where("game_type like ? AND zip like ?", "%#{query1}%", "%#{query3}%")
+  		
+  		elsif (query1 == "" && query2 != "" && query3 == "" && query4 != "") #A user searches for a match by creator and date.
+  			where("player1 like ? AND time like ?", User.find_by(name: "#{query2}").id, "%#{query4}%")
+  		
+  		elsif (query1 == "" && query2 == "" && query3 != "" && query4 != "") #A user searches for a match by zip and date.
+  			where("zip like ? AND time like ?", "%#{query3}%", "%#{query4}%")
+
+  		elsif (query1 != "" && query2 != "" && query3 != "" && query4 == "") #A user searches for a match by type, creator and zip. 
+  			where("game_type like ? AND player1 like ? AND zip like ?", "%#{query1}%", User.find_by(name: "#{query2}").id, "%#{query3}%")
+
+  		elsif (query1 != "" && query2 == "" && query3 != "" && query4 != "") #A user searches for a match by type, zip and date.
+  			where("game_type like ? AND zip like ? AND time like ?", "%#{query1}%", "%#{query3}%", "%#{query4}%")
+
+  		elsif (query1 != "" && query2 != "" && query3 == "" && query4 != "") #A user searches for a match by type, creator and date.
+  			where("game_type like ? AND player1 like ? AND time like ?", "%#{query1}%", User.find_by(name: "#{query2}").id, "%#{query4}%")
   		else
       		where("open = ?", 1)
       	end
-	  #where("game_type like ?", "%#{query}%") 
+
 	end
 
 	#def self.search1(query1)
