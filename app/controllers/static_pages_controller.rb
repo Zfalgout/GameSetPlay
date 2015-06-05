48a4b2e9c31d71afc92ee1c47a49d655f454a4f8
unknown <zfalgout@DSMWIN8.helena.com>
2015-06-05 10:24:57 -0500

    Updated the match feed to include time constrictions.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @feed_items = current_user.feed.where("p2Active = ? AND p3Active = ? AND p4Active = ?", 1, 1, 1).paginate(page: params[:page]).order(time: :desc)
+      @feed_items = current_user.feed.where("p2Active = ? AND p3Active = ? AND p4Active = ?", 1, 1, 1).where(time: 10.years.ago..1.month.from_now).paginate(page: params[:page]).order(time: :desc)
     end
   end
 
